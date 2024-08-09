ac9d49d0038205bff8fe08446a964b2c04fa1e5e
陈千瓦时的 <chenqianwashide@192.168.5.66>
2024-08-09 18:21:23 +0800

    export

--- src/components/ProxyList.js
+++ src/components/ProxyList.js
@@ -2,7 +2,6 @@ import { useState, useEffect, useRef, useMemo } from "react";
 import { AnonymityLevelsText } from "@/util/enums";
 import { supabase, getPagination } from "@/util";
 import { useRouter } from "next/router";
-
 import styles from "@/styles/freeProxy.module.css";
 
 function ProxyList({ initProxyList, options, initCount }) {
@@ -89,6 +88,40 @@ function ProxyList({ initProxyList, options, initCount }) {
     setCount(newCount);
   };
 
+  function convertDataToCSV(data) {
+    const header = Object.keys(data[0]).join(",") + "\n"; // 提取第一个对象的键作为 CSV 表头
+    const rows = data.map((obj) => Object.values(obj).join(",")); // 提取每个对象的值并用逗号连接
+
+    return header + rows.join("\n"); // 将表头和行连接起来，使用换行符分隔
+  }
+
+  const handleExport = () => {
+    const jsonStr = JSON.stringify(proxyList);
+    const text = JSON.stringify(proxyList);
+    const csv = convertDataToCSV(proxyList);
+    let blob;
+    if (exportType === ExportTypes.Json) {
+      blob = new Blob([jsonStr], { type: "application/json" });
+    } else if (exportType === ExportTypes.Csv) {
+      blob = new Blob([csv], { type: "text/csv" });
+    } else if (exportType === ExportTypes.Txt) {
+      blob = new Blob([text], { type: "text/plain" });
+    }
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+
+    if (exportType === ExportTypes.Json) {
+      link.download = "data.json";
+    } else if (exportType === ExportTypes.Csv) {
+      link.download = "data.csv";
+    } else if (exportType === ExportTypes.Txt) {
+      link.download = "data.txt";
+    }
+    link.click();
+    URL.revokeObjectURL(url);
+  };
+
   useEffect(() => {
     // 初始加载时不触发数据获取逻辑
     if (!initialLoad.current) {
@@ -100,7 +133,7 @@ function ProxyList({ initProxyList, options, initCount }) {
     } else {
       initialLoad.current = false;
     }
-  }, [currentPage,slug]);
+  }, [currentPage, slug]);
 
   const tableRows = proxyList.map((item, index) => (
     <tr key={index}>
@@ -168,14 +201,21 @@ function ProxyList({ initProxyList, options, initCount }) {
                     className={`${
                       exportType === ExportTypes[i] ? styles.activeBtn : ""
                     }`}
-                    onClick={() => setExportType(ExportTypes[i])}
+                    onClick={() => {
+                      setExportType(ExportTypes[i]);
+                    }}
                     key={i}
                   >
                     {ExportTypes[i]}
                   </button>
                 );
               })}
-              <button className={styles.downloadBtn}>Download</button>
+              <button
+                onClick={() => handleExport()}
+                className={styles.downloadBtn}
+              >
+                Download
+              </button>
             </div>
             <table className={styles.fpTable}>
               <thead className={styles.fpThead}>
